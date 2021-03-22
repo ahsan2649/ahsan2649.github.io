40eefc4230b5bbec75ae2c1ec0810af038a8bea7
Alif Ahsan Nibir <alifahsan.2649@gmail.com>
2021-03-22 16:36:24 +0600

    working on the navbar

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,20 @@
 App.component('nav-bar', {
     template:
         /*html*/
-        `<div class="nav-bar"></div>`,
+        `<div class="nav-bar">
+            <ul style="list-style-type:none">
+                <li v-for="pic in pics">{{pic.name}}</li>
+            </ul>
+        </div>`,
     data(){
         return{
-
+            pics: [
+                { name: 'button' },
+                { name: 'button' },
+                { name: 'button' },
+                { name: 'button' },
+                { name: 'button' },
+                { name: 'button' }]
         }
     }
 })
