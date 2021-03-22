db901fc5c7a227ce2bde70c110d2f77c102d82cb
Alif Ahsan Nibir <alifahsan.2649@gmail.com>
2021-03-23 00:39:36 +0600

    Putting everything in one app
    Gonna separate into other apps later

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const app = {
     data() {
         return {
-            debug: 'This is the start of the app'
+            debug: {
+                nav: ['item', 'item', 'item', 'item', 'item'],
+                selectedItem: 0,
+                items: [{
+                        title: 'title',
+                        carousel: 'carousel',
+                        description: 'description'
+                },
+                    {
+                    title: 'title',
+                    carousel: 'carousel',
+                    description: 'description'
+                },
+                    {
+                    title: 'title',
+                    carousel: 'carousel',
+                    description: 'description'
+                },
+                    {
+                    title: 'title',
+                    carousel: 'carousel',
+                    description: 'description'
+                    },
+                    {
+                    title: 'title',
+                    carousel: 'carousel',
+                    description: 'description'
+                }]
+            }
         }
     }
 }
